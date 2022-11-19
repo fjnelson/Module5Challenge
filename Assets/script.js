83e3d0aaaf95117f009e0f5902d91e56e36569fb
fjnelson <97076779+fjnelson@users.noreply.github.com>
2022-11-18 20:46:21 -0800

    updated functions

--- Assets/script.js
+++ Assets/script.js
@@ -1,70 +1,51 @@
 //contribution: https://github.com/mmeii/work-day-scheduler
 
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-
-
+//TODO: wrapping function to ensure code doesn't run until browser renders
+$(function () {
   //variables
   var saveBtn = $(".saveBtn");
 
   //functions
-  $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
+
+  //TODO: display current date
+  $("#currentDay").text(moment().format("dddd MMMM Do YYYY"));
 
   function timeBlockColor() {
     var hour = moment().hours();
 
-    $(".time-block").each(function() {
-        var currHour = parseInt($(this).attr("id"));
+    $(".time-block").each(function () {
+      var currHour = parseInt($(this).attr("id"));
+      // TODO: Adding past, present, or future class to each time
+      if (currHour > hour) {
+        $(this).addClass("future");
+      } else if (currHour === hour) {
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("past");
+      }
+    });
+  }
 
-        if (currHour > hour) {
-            $(this).addClass("future");
-        } else if (currHour === hour) {
-            $(this).addClass("present");
-        } else {
-            $(this).addClass("past");
-        }
-    })
-};
-
-saveBtn.on("click", function() {
-
+  //TODO: click event listener using 'this'
+  saveBtn.on("click", function () {
     var time = $(this).siblings(".hour").text();
     var plan = $(this).siblings(".plan").val();
 
     localStorage.setItem(time, plan);
-});
+  });
 
-function usePlanner() {
+  function usePlanner() {
+    //TODO: setting value of local storage
+    $(".hour").each(function () {
+      var currHour = $(this).text();
+      var currPlan = localStorage.getItem(currHour);
 
-    $(".hour").each(function() {
-        var currHour = $(this).text();
-        var currPlan = localStorage.getItem(currHour);
-
-        if(currPlan !== null) {
-            $(this).siblings(".plan").val(currPlan);
-        }
+      if (currPlan !== null) {
+        $(this).siblings(".plan").val(currPlan);
+      }
     });
-}
+  }
 
-timeBlockColor();
-usePlanner();
+  timeBlockColor();
+  usePlanner();
+});
