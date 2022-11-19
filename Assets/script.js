349fc9e0425b754c97d94bd35752bbde42feb796
fjnelson <97076779+fjnelson@users.noreply.github.com>
2022-11-18 20:33:54 -0800

    adding HTML, CSS, JS

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,9 @@
+//contribution: https://github.com/mmeii/work-day-scheduler
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +22,49 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+
+
+  //variables
+  var saveBtn = $(".saveBtn");
+
+  //functions
+  $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
+
+  function timeBlockColor() {
+    var hour = moment().hours();
+
+    $(".time-block").each(function() {
+        var currHour = parseInt($(this).attr("id"));
+
+        if (currHour > hour) {
+            $(this).addClass("future");
+        } else if (currHour === hour) {
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("past");
+        }
+    })
+};
+
+saveBtn.on("click", function() {
+
+    var time = $(this).siblings(".hour").text();
+    var plan = $(this).siblings(".plan").val();
+
+    localStorage.setItem(time, plan);
 });
+
+function usePlanner() {
+
+    $(".hour").each(function() {
+        var currHour = $(this).text();
+        var currPlan = localStorage.getItem(currHour);
+
+        if(currPlan !== null) {
+            $(this).siblings(".plan").val(currPlan);
+        }
+    });
+}
+
+timeBlockColor();
+usePlanner();
